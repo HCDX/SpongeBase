73415028ad25b45dcaab5ee58d59d29bb2149af6
jamescw <jcranwellward@gmail.com>
2014-06-05 15:04:50 +0300

    Added spongemap app

---
+++ spongemap.py
@@ -0,0 +1,27 @@
+import os
+from flask import (
+    Flask,
+    render_template
+)
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('DOG_SETTINGS', silent=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
