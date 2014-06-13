76f922c003dafd9f4acb8187a33b30bcc09275f5
Jad Ghosn <jadghosn@gmail.com>
2014-06-13 14:21:25 +0300

    Added Admin Level 4 Layer
    Fixed layer control menu. Layers are off on load.

---
+++ templates/App.js
@@ -0,0 +1,13 @@
+﻿function GetLayer(adminLayer) {
+    // get sublayer 0 and set the infowindow template
+    adminLayer.getSubLayer(0);
+    var adm1layer = adminLayer.getSubLayer(0);
+    adm1layer.infowindow.set('template', $('#infowindow_template').html());
+}
+
+function addadm1layer(map, adminLayer, layerurl) {
+    adminLayer = cartodb.createLayer(map, layerurl).addTo(map).on('done', 'nothing');
+    var adm1layer = adminLayer.getSubLayer(0);
+    //GetLayer(adminLayer, adminLayer);
+    createSelector('adm1');
+}
