b29d676597f9aa48042be56b80b87ed9df02dea7
jamescw <jcranwellward@gmail.com>
2014-09-22 17:01:19 +0300

    covert port int

--- spongemap/spongemap/settings.py
+++ spongemap/spongemap/settings.py
@@ -75,7 +75,7 @@ connect(
     username=os.environ.get('MONGODB_USERNAME'),
     password=os.environ.get('MONGODB_PASSWORD'),
     host=os.environ.get('MONGODB_HOST'),
-    port=os.environ.get('MONGODB_PORT'),
+    port=os.environ.get(int('MONGODB_PORT')),
 )
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
