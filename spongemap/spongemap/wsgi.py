e52666857a3625c565bfe82b6250c251d687e3b7
jamescw <jcranwellward@gmail.com>
2014-09-24 23:06:44 +0300

    use dj-static

--- spongemap/spongemap/wsgi.py
+++ spongemap/spongemap/wsgi.py
@@ -17,4 +17,6 @@ path.append(SITE_ROOT)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spongemap.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
