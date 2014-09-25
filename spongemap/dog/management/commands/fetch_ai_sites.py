32f23437789031fbc383143468aaf634935e5a1e
jamescw <jcranwellward@gmail.com>
2014-09-25 17:07:15 +0300

    fix encoding issues

--- spongemap/dog/management/commands/fetch_ai_sites.py
+++ spongemap/dog/management/commands/fetch_ai_sites.py
@@ -14,33 +14,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        try:
+        ai = MongoClient(
+            os.environ.get('AI_MONGO_URL', 'mongodb://localhost:27017'))[
+            os.environ.get('AI_MONGODB_DATABASE', 'ai')]
 
-            ai = MongoClient(
-                os.environ.get('AI_MONGO_URL', 'mongodb://localhost:27017'))[
-                os.environ.get('AI_MONGODB_DATABASE', 'ai')]
+        for report in ai.report.find({'p_code': {'$ne': None}}):
+            fact, created = Fact.objects.get_or_create(source_id=str(report['_id']))
+            fact.source = 'ActivityInfo: {}'.format(report['partner_name'])
+            fact.date = datetime.strptime(report['date'], '%Y-%m')
+            fact.code = report['p_code']
+            fact.description = report['indicator_name']
+            fact.value = str(report['value'])
+            fact.category = report['activity']
+            fact.created_on = datetime.now()
 
-            for report in ai.report.find({'p_code': {'$ne': None}}):
-                fact, created = Fact.objects.get_or_create(source_id=str(report['_id']))
-                fact.source = 'ActivityInfo: {}'.format(report['partner_name'])
-                fact.date = datetime.strptime(report['date'], '%Y-%m')
-                fact.code = report['p_code']
-                fact.description = report['indicator_name']
-                fact.value = str(report['value'])
-                fact.category = report['activity']
-                fact.created_on = datetime.now()
+            fact.save()
 
-                fact.save()
-
-                if created:
-                    print 'Created fact: {} -> {} -> {} -> {} = {}'.format(
-                        fact.date,
-                        fact.source,
-                        fact.category,
-                        fact.description,
-                        fact.value
-                    ).encode('utf-8')
-
-        except Exception as exp:
-            raise CommandError(exp)
+            if created:
+                print 'Created fact: {} -> {} -> {} -> {} = {}'.format(
+                    fact.date,
+                    fact.source,
+                    fact.category.encode('utf-8'),
+                    fact.description.encode('utf-8'),
+                    fact.value
+                )
 
