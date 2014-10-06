9619e3d262b86596a682d19318bf9d092f73561a
jamescw <jcranwellward@gmail.com>
2014-10-06 23:40:37 +0300

    lock down

--- spongemap/spongemap/urls.py
+++ spongemap/spongemap/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'spongemap.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='spongemap.html')),
+    url(r'^$', login_required(TemplateView.as_view(template_name='spongemap.html'))),
 
     url(r'^facts/$', FactsView.as_view()),
 
