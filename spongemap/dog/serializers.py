948ab46e72bd223fe9cc0123458e7f38daa63041
Jad Ghosn <jadghosn@gmail.com>
2015-02-26 15:52:56 +0200

    Changed info window from Popup to right side panel

--- spongemap/dog/serializers.py
+++ spongemap/dog/serializers.py
@@ -1,11 +1,11 @@
 __author__ = 'jcranwellward'
 
-from rest_framework_mongoengine.serializers import MongoEngineModelSerializer
+from rest_framework_mongoengine.serializers import DocumentSerializer
 
 from .models import Fact
 
 
-class FactSerializer(MongoEngineModelSerializer):
+class FactSerializer(DocumentSerializer):
 
     class Meta:
         model = Fact
