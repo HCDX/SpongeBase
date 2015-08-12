c7c71580f4aaa6b3c935101c14d031114cddcae4
nmt19 <nour_tabaja@hotmail.com>
2015-08-12 15:29:40 +0300

    new js

--- static/js/spongemap.js
+++ static/js/spongemap.js
@@ -18,6 +18,7 @@
         var itson = 0;
         var schoolon = 0;
 		var phcon = 0;
+		var shcon = 0;
 		var municipalitieson = 0;
         var adm1layer;
         var adm2layer;
@@ -26,18 +27,19 @@
         var itslayer;
         var schoolslayer;
 		var phclayer;
+		var shclayer;
 		var municipalitieslayer;
         var itslayerurl = 'http://unhcr.cartodb.com/api/v2/viz/6d01148e-ebbe-11e3-8ccb-0e230854a1cb/viz.json';
-        var adm1layerurl = 'http://unhcr.cartodb.com/api/v2/viz/37b3bf66-ed76-11e3-abe6-0e230854a1cb/viz.json';
+        var adm1layerurl = 'https://unhcr.cartodb.com/api/v2/viz/37b3bf66-ed76-11e3-abe6-0e230854a1cb/viz.json';
         var adm2layerurl = 'http://unhcr.cartodb.com/api/v2/viz/80a4de6a-f2eb-11e3-aea2-0e230854a1cb/viz.json';
         var adm3layerurl = 'http://unhcr.cartodb.com/api/v2/viz/2a7fe946-efe7-11e3-abb9-0e73339ffa50/viz.json';
         var adm4layerurl = 'http://unhcr.cartodb.com/api/v2/viz/46e8c810-4f9a-11e4-bbdf-0e4fddd5de28/viz.json';
         var schoolslayerurl = 'http://unhcr.cartodb.com/api/v2/viz/430d4bb0-face-11e3-ac1a-0e230854a1cb/viz.json';
 		var phclayerurl='http://unhcr.cartodb.com/api/v2/viz/4704f114-bf68-11e4-a773-0e0c41326911/viz.json';
+		var shclayerurl='https://unhcr.cartodb.com/api/v2/viz/f4eea356-36a5-11e5-be09-0e9d821ea90d/viz.json';
 		var municipalitieslayerurl='http://unhcr.cartodb.com/api/v2/viz/497cfb5a-d31b-11e4-ace7-0e0c41326911/viz.json';
 
 $(document).ready( function(){
-
     map = L.map('map', {
         zoomControl: true,
         center: [34, 36.1],
@@ -58,11 +60,86 @@ $(document).ready( function(){
     //addadm1layer(map, adm1layer, adm1layerurl);
     additslayer(itslayerurl);
     addschoolslayer(schoolslayerurl);
-    addphclayer(phclayerurl);
+    addphclayer(phclayerurl); 
+	addshclayer(shclayerurl);
     addmunicipalitieslayer(municipalitieslayerurl);
 
 });
-
+	
+ var INFOWINDOW_TEMPLATE = [
+        '<div class="cartodb-popup v2">',
+        '  <a href="#close" class="cartodb-popup-close-button close">x</a>',
+        '   <div class="cartodb-popup-content-wrapper">',
+        '     <div class="cartodb-popup-content">',
+        '       <div id="chart_div">',
+        '       <script>',
+        '         draw_chart([{{content.data.jan_31_2013}},{{content.data.feb_28_2013}}, {{content.data.mar_31_2013}},{{content.data.apr_30_2013}},{{content.data.may_31_2013}},{{content.data.jun_30_2013}},{{content.data.jul_21_2013}},{{content.data.aug_31_2013}},{{content.data.sep_30_2013}},{{content.data.oct_31_2013}},{{content.data.nov_30_2013}},{{content.data.dec_31_2013}},{{content.data.jan_30_2014}},{{content.data.feb_28_2014}},{{content.data.march_31_2014}},{{content.data.april_30_2014}},{{content.data.may_31_2014}},{{content.data.jun_30_2014}},{{content.data.july_31_2014}},{{content.data.aug_31_2014}},{{content.data.sep_30_2014}},{{content.data.oct_31_2014}},{{content.data.nov_30_2014}},{{content.data.dec_31_2014}},{{content.data.jan_31_2015}},{{content.data.feb_28_2015}},{{content.data.march_31_2015}},{{content.data.apr_30_2015}},{{content.data.may_31_2015}},{{content.data.jun_30_2015}}], "{{content.data.shape_leng}}");',
+        '       </scr' + 'ipt>',
+        '       </div>',
+        '     </div>',
+        '   </div>',
+        '   <div class="cartodb-popup-tip-container"></div>',
+        '</div>'].join('');
+		
+		
+		 var INFOWINDOW_TEMPLATE1 = [
+        '<div class="cartodb-popup v2">',
+        '  <a href="#close" class="cartodb-popup-close-button close">x</a>',
+        '   <div class="cartodb-popup-content-wrapper">',
+        '     <div class="cartodb-popup-content">',
+        '       <div id="chart_div">',
+        '       <script>',
+        '         draw_chart([{{content.data.jan_31_2013}},{{content.data.feb_28_2013}}, {{content.data.mar_31_2013}},{{content.data.apr_30_2013}},{{content.data.may_31_2013}},{{content.data.jun_30_2013}},{{content.data.jul_21_2013}},{{content.data.aug_31_2013}},{{content.data.sep_30_2013}},{{content.data.oct_31_2013}},{{content.data.nov_30_2013}},{{content.data.dec_31_2013}},{{content.data.jan_30_2014}},{{content.data.feb_28_2014}},{{content.data.march_31_2014}},{{content.data.april_30_2014}},{{content.data.may_31_2014}},{{content.data.jun_30_2014}},{{content.data.july_31_2014}},{{content.data.aug_31_2014}},{{content.data.sep_30_2014}},{{content.data.oct_31_2014}},{{content.data.nov_30_2014}},{{content.data.dec_31_2014}},{{content.data.jan_31_2015}},{{content.data.feb_28_2015}},{{content.data.march_31_2015}},{{content.data.apr_30_2015}},{{content.data.may_31_2015}},{{content.data.jun_30_2015}}], "{{content.data.shape_leng}}");',
+        '       </scr' + 'ipt>',
+        '       </div>',
+        '     </div>',
+        '   </div>',
+        '   <div class="cartodb-popup-tip-container"></div>',
+        '</div>'].join('');
+	
+      // load visualization library from google
+      google.load('visualization', '1.0', {'packages':['corechart']});
+      function draw_chart(data, name) {
+         var data = google.visualization.arrayToDataTable([
+            ['', 'population'],
+            ['Jan 2013',  data[0]],
+            ['Feb 2013',  data[1]],
+			['Mar 2013',  data[2]],
+			['Apr 2013',  data[3]],
+			['May 2013',  data[4]],
+			['Jun 2013',  data[5]],
+			['Jul 2013',  data[6]],
+			['Aug 2013',  data[7]],
+			['Sep 2013',  data[8]],
+			['Oct 2013',  data[9]],
+			['Nov 2013',  data[10]],
+			['Dec 2013',  data[11]],
+			['Jan 2014',  data[12]],
+			['Feb 2014',  data[13]],
+			[' Mar 2014',  data[14]],
+			[' Apr 2014',  data[15]],
+			['May 2014',  data[16]],
+			['Jun 2014',  data[17]],
+			['Jul 2014',  data[18]],
+			['Aug 2014',  data[19]],
+			['Sep 2014',  data[20]],
+			['Oct 2014',  data[21]],
+			['Nov 2014',  data[22]],
+			['Dec 2014',  data[23]],
+			//['Jan 2015',  data[24]],
+			//['Feb 2015',  data[25]],
+			//['Mar 2015',  data[26]],
+			//['Apr 2015',  data[27]],
+			//['May 2015',  data[28]]
+         ]);
+          var options = {
+            title:  '	Refugees TimeLine',
+            legend: { position: "none" },
+            width: 300,
+          };
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+          chart.draw(data, options);
+      }
 
 function createSelector(origin) {
 
@@ -122,7 +199,10 @@ function createSelector(origin) {
         }
 
         if (origin == 'adm2') {
+		
+
             if (area == '2' && adm2on == 0) {
+				
                 //show adm3 layer
                 adm2on = 1;
                 adm2layer.show();
@@ -135,9 +215,10 @@ function createSelector(origin) {
                 adm2on = 0;
             }
         }
-
-
+	
+		
         if (origin == 'its') {
+
             if (area == '5' && itson == 0) {
                 //show adm1 layer
                 itson = 1;
@@ -145,17 +226,22 @@ function createSelector(origin) {
                 $li.addClass('selected');
             }
             else if (area == '5' && itson == 1) {
+			
                 //hide layer
                 $li.removeClass('selected');
                 itslayer.hide();
                 itson = 0;
             }
         }
+		
         if (origin == 'schools') {
+		
             if (area == '6' && schoolon == 0) {
+		
                 //show adm1 layer
                 schoolon = 1;
                 schoolslayer.show();
+				
                 $li.addClass('selected');
             }
             else if (area == '6' && schoolon == 1) {
@@ -167,7 +253,9 @@ function createSelector(origin) {
         }
 
           if (origin == 'phc') {
+
             if (area == '7' && phcon == 0) {
+
                 //show adm1 layer
                 phcon = 1;
                 phclayer.show();
@@ -182,7 +270,10 @@ function createSelector(origin) {
         }
 
          if (origin == 'municipalities') {
+		
             if (area == '8' && municipalitieson == 0) {
+		
+				
                 //show adm1 layer
                 municipalitieson = 1;
                 municipalitieslayer.show();
@@ -195,13 +286,84 @@ function createSelector(origin) {
                 municipalitieson = 0;
             }
         }
-
+		
         selectedli = area;
         // change the query in the layer to update the map
 
     });
 }
+	
 
+function createSelector2(origin) {
+
+    var $options = $('#layer_selector li li');
+    $options.click(function (e) {
+        // get the area of the selected layer
+        var $li = $(e.target);
+        var area = $li.attr('data');
+
+        // deselect all and select the clicked one
+        //$options.removeClass('selected');
+
+        //$li.addClass('selected');
+
+        if (origin == 'schools') {
+            if (area == '6' && schoolon == 0) {
+				$("#education").click(function(){
+		$('nav ul li:hover > ul').css('display', 'block');
+	});
+                //show adm1 layer
+                schoolon = 1;
+                schoolslayer.show();
+                $li.addClass('selected');
+            }
+            else if (area == '6' && schoolon == 1) {
+                //hide layer
+                $li.removeClass('selected');
+                schoolslayer.hide();
+                schoolon = 0;
+            }
+        }
+          if (origin == 'phc') {
+            if (area == '7' && phcon == 0) {
+					$("#health").click(function(){
+		$('nav ul li:hover > ul').css('display', 'block');
+	});
+                //show adm1 layer
+                phcon = 1;
+                phclayer.show();
+                $li.addClass('selected');
+            }
+            else if (area == '7' && phcon == 1) {
+				
+                //hide layer
+                $li.removeClass('selected');
+                phclayer.hide();
+                phcon = 0;
+            }
+        }
+		if (origin == 'shc') {
+            if (area == '9' && shcon == 0) {
+					$("#health").click(function(){
+		$('nav ul li:hover > ul').css('display', 'block');
+	});
+                //show adm1 layer
+                shcon = 1;
+                shclayer.show();
+                $li.addClass('selected');
+         }
+            else if (area == '9' && shcon == 1) {
+                //hide layer
+                $li.removeClass('selected');
+                shclayer.hide();
+                shcon = 0;
+            }
+        }
+        selectedli = area;
+        // change the query in the layer to update the map
+
+    });
+}
 function clearMap() {
     for (i in map._layers) {
         try {
@@ -214,7 +376,6 @@ function clearMap() {
         }
     }
 }
-
 function main() {
 
 
@@ -222,13 +383,8 @@ function main() {
     //L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
     //    attribution: 'Stamen'
     //}).addTo(map);
-
-
-
 }
 //Custom Layer
-
-
 //function to add new adm1 layer
 function addadm1layer(layerurl) {
     cartodb.createLayer(map, layerurl).addTo(map)
@@ -236,17 +392,20 @@ function addadm1layer(layerurl) {
          function (layer) {
              // get sublayer 0 and set the infowindow template
              adm1layer = layer.getSubLayer(0);
-             adm1layer.infowindow.set('template', $('#infowindow_template').html());
+              adm1layer.infowindow.set({
+              template:   INFOWINDOW_TEMPLATE,
+              sanitizeTemplate: false,
+              width:      328,
+              maxHeight:  400
+            });
+			 //adm1layer.legend=true;
              adm1layer.hide();
+			 
          }).on('error', function () {
              console.log("some error occurred");
          });
-
-
     createSelector('adm1');
-
 }
-
 //function to add new adm1 layer
 function addadm2layer(layerurl) {
     cartodb.createLayer(map, layerurl).addTo(map)
@@ -254,15 +413,17 @@ function addadm2layer(layerurl) {
          function (layer) {
              // get sublayer 0 and set the infowindow template
              adm2layer = layer.getSubLayer(0);
-             adm2layer.infowindow.set('template', $('#infowindow_template').html());
+              adm2layer.infowindow.set({
+              template:   INFOWINDOW_TEMPLATE1,
+              sanitizeTemplate: false,
+              width:      328,
+              maxHeight:  400
+            });
              adm2layer.hide();
          }).on('error', function () {
              console.log("some error occurred");
          });
-
-
     createSelector('adm2');
-
 }
 
 function addadm4layer(layerurl) {
@@ -280,13 +441,8 @@ function addadm4layer(layerurl) {
          }).on('error', function () {
              console.log("some error occurred");
          });
-
-
     createSelector('adm4');
-
 }
-
-
 function addadm3layer(layerurl) {
     cartodb.createLayer(map, layerurl).addTo(map)
          .on('done',
@@ -304,8 +460,6 @@ function addadm3layer(layerurl) {
          }).on('error', function () {
              console.log("some error occurred");
          });
-
-
     createSelector('adm3');
 }
 
@@ -320,20 +474,15 @@ function additslayer(layerurl) {
                  //  alert("You clicked " + data.p_code);
                  $(function () {
                      Fetch(data.pcode, data.pcode_name);
-
                  });
                  fetchTweets(data.pcode);
              });
-
-
              itslayer.hide();
          }).on('error', function () {
              console.log("some error occurred");
          });
 
     createSelector('its');
-
-
 }
 function addschoolslayer(layerurl) {
     cartodb.createLayer(map, layerurl).addTo(map)
@@ -345,7 +494,6 @@ function addschoolslayer(layerurl) {
                  //  alert("You clicked " + data.p_code);
                  $(function () {
                      Fetch(data.cerd, data.school_name);
-
                  });
                  fetchTweets(data.cerd);
              });
@@ -353,10 +501,8 @@ function addschoolslayer(layerurl) {
          }).on('error', function () {
              console.log("some error occurred");
          });
-
-    createSelector('schools');
+    createSelector2('schools');
 }
-
  function addphclayer(layerurl) {
     cartodb.createLayer(map, layerurl).addTo(map)
          .on('done',
@@ -376,7 +522,28 @@ function addschoolslayer(layerurl) {
              console.log("some error occurred");
          });
 
-    createSelector('phc');
+    createSelector2('phc');
+} 
+function addshclayer(layerurl) {
+    cartodb.createLayer(map, layerurl).addTo(map)
+         .on('done',
+         function (layer) {
+
+             shclayer = layer.getSubLayer(0);
+             shclayer.on('featureClick', function (e, pos, latlng, data) {
+                 //  alert("You clicked " + data.p_code);
+                 $(function () {
+                     Fetch(""+ data.p_code, data.name_of_health_facility);
+
+                 });
+                 fetchTweets(data.p_code);
+             });
+             shclayer.hide();
+         }).on('error', function () {
+             console.log("some error occurred");
+         });
+
+    createSelector2('shc');
 }
 
  function addmunicipalitieslayer(layerurl) {
@@ -400,12 +567,10 @@ function addschoolslayer(layerurl) {
 
     createSelector('municipalities');
 }
-
 window.onload = main;
-
 function Fetch(pcode, name) {
     $.ajax({
-        url: "http://ai-aggregator.apps.uniceflebanon.org/reports/?p_code=" + pcode,
+         url: "http://ai-aggregator.apps.uniceflebanon.org/reports/?p_code=" + pcode,
         //dataType: "jsonp",
         success: function (retdata, statuss) {
             var marker_content = '\
@@ -421,8 +586,6 @@ function Fetch(pcode, name) {
                           </tr>\
             </thead>\
             <tbody>\ ';
-
-
             for (var i = 0; i < retdata.data.length; i++) {
                 var obj = retdata.data[i];
 				var cat = obj.category; 
@@ -451,20 +614,39 @@ function Fetch(pcode, name) {
                 sortRestart: true,
                 sortList: [[0,1], [1,0]]
             });
-
             $("table").addTableFilter();
             }
     });
-
-
 }
+ $('#searchTxt').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)  // the enter key code
+  {
+    $('#btnSearch').click();
+    return false;  
+  }
+});
 
 function processFormData() {
 return document.getElementById('searchTxt').value;
 }
-
+function joinTables()
+{
+	var sql = new cartodb.SQL({ user: 'unhcr' });
+	sql.execute("SELECT * FROM sponge_lbn_adm2s WHERE gov_code > {{id}}", { id: 3 })
+  .done(function(data) {
+    console.log(data.rows);
+  })
+  .error(function(errors) {
+    // errors contains a list of errors
+    console.log("errors:" + errors);
+  })
+	//joinTables();
+}
+/*SELECT m_ref_name, date_text,sum(refugees) FROM refugees_time_line_by_district 
+group by m_ref_name,date_text
+*/
 function searchFunction(){
-
 var mySearch = processFormData();
 var sql = new cartodb.SQL({ user: 'unhcr' });
 sql.getBounds("SELECT * FROM ai_allsites where lower(pcode) LIKE lower('%" + mySearch + "%') OR lower(site_name) LIKE lower('%" +
@@ -475,9 +657,3 @@ mySearch + "%')").done(function(bounds) {
     }
 });
 }
-
-
-
-
-
-
