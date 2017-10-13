9da4565a15a43eb01fdaafe677e9ede25da2281c
John Dingee <dingeej@gmail.com>
2017-10-13 13:24:17 -0400

    add hide button for info panel

--- frontend/web-sponge/src/components/InfoPanel.js
+++ frontend/web-sponge/src/components/InfoPanel.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
 
+import { Button } from 'reactstrap'
+
 class InfoPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hidden: false,
+    }
+  }
 
   render() {
+        const hideButton = <Button
+            onClick={(e)=> this.setState({hidden: !this.state.hidden})}>
+            {this.state.hidden ? 'show' : 'hide'}
+          </Button>
 
-    return (
-      <div className="sidepanel">
-          <div className="wrapper">
-              <div className="context subheader">
-                  <p>A joint UNHCR/UNICEF innovation project</p>
+        const pageContent = this.state.hidden ? hideButton
+          : <div className="sidepanel">
+              {hideButton}
+              <div className="wrapper">
+                  <div className="context subheader">
+                      <p>A joint UNHCR/UNICEF innovation project</p>
+                  </div>
+                  <h1 className="text-muted">SpongeBase visualization</h1>
+                  <p>The number of Syrians registered as refugees in Lebanon after fleeing war in their country has surpassed one million. Refugees from Syria, now equal a quarter of Lebanon's resident population. Most of them live in poverty and depend on aid for survival.</p>
+
+                  <h4 className="text-muted">DESCRIPTION</h4>
+                  <p>This interactive map collates available information from different data sources and links them to a location whether a governorate,a district, a village or even a small camp.</p>
+                  <h4 className="text-muted">SOURCES</h4>
+                  <p>
+                      <a href="http://data.unhcr.org/syrianrefugees/country.php?id=122">UNHCR Data Portal</a>
+                      <a href="http://activityinfo.org">Activity Info</a>
+                  </p>
               </div>
-              <h1 className="text-muted">SpongeBase visualization</h1>
-              <p>The number of Syrians registered as refugees in Lebanon after fleeing war in their country has surpassed one million. Refugees from Syria, now equal a quarter of Lebanon's resident population. Most of them live in poverty and depend on aid for survival.</p>
-
-              <h4 className="text-muted">DESCRIPTION</h4>
-              <p>This interactive map collates available information from different data sources and links them to a location whether a governorate,a district, a village or even a small camp.</p>
-              <h4 className="text-muted">SOURCES</h4>
-              <p>
-                  <a href="http://data.unhcr.org/syrianrefugees/country.php?id=122">UNHCR Data Portal</a>
-                  <a href="http://activityinfo.org">Activity Info</a>
-              </p>
-          </div>
-      <div id="page-wrap" className="wrapper">
-              <ul className="nav nav-tabs">
-                <li className="active"><a href="#ai_data" data-toggle="tab">Humanitarian Data</a></li>
-                <li><a href="#social" data-toggle="tab">Social</a></li>
-              </ul>
-              <div className="tab-content">
-                  <div id="ai_data" className="tab-pane active"></div>
-                  <div id="social" className="tab-pane">
-                      <div id="twitter-feed"></div>
+              <div id="page-wrap" className="wrapper">
+                  <ul className="nav nav-tabs">
+                    <li className="active"><a href="#ai_data" data-toggle="tab">Humanitarian Data</a></li>
+                    <li><a href="#social" data-toggle="tab">Social</a></li>
+                  </ul>
+                  <div className="tab-content">
+                      <div id="ai_data" className="tab-pane active"></div>
+                      <div id="social" className="tab-pane">
+                          <div id="twitter-feed"></div>
+                      </div>
                   </div>
               </div>
+          <div className="context footer">
+                  <p>Visit the Inter-agency information sharing webportal <a href="http://data.unhcr.org/syrianrefugees/country.php?id=122">Here</a></p>
+              </div>
           </div>
-      <div className="context footer">
-              <p>Visit the Inter-agency information sharing webportal <a href="http://data.unhcr.org/syrianrefugees/country.php?id=122">Here</a></p>
-          </div>
-      </div>
-    )
+
+    return pageContent
+
+
   }
 }
 
